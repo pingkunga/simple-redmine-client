5111d444b9532d922ff6ad70b133b4d8c796f790
Chatri Ngambenchawong <pingkunga@gmail.com>
2024-12-31 06:34:05 +0700

    feat: add sidebar

---
+++ components/sidebar/Sidebar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="scrollnavbar">
+      <v-list class="pa-4">
+        <!-- ---------------------------------------------- -->
+        <!---Menu Loop -->
+        <!-- ---------------------------------------------- -->
+        <template v-for="(item, i) in sidebarMenu" :key="i">
+          <!-- ---------------------------------------------- -->
+          <!---Single Item-->
+          <!-- ---------------------------------------------- -->
+          <v-list-item :to="item.to" rounded="lg" class="mb-1">
+            <template v-slot:prepend>
+              <v-icon :icon="item.icon"></v-icon>
+            </template>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item>
+        </template>
+      </v-list>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import sidebarItems from "./sidebarItems";
+const sidebarMenu = ref(sidebarItems);
+</script>
+
+<style scoped></style>
---
+++ components/sidebar/sidebarItems.ts
@@ -0,0 +1,17 @@
+export default [
+    {
+      title: "Home",
+      icon: "mdi-view-dashboard-outline",
+      to: "/",
+    },
+    {
+      title: "Versions List",
+      icon: "mdi-storefront-outline",
+      to: "/versions",
+    },
+    {
+      title: "Issues List",
+      icon: "mdi-format-list-bulleted",
+      to: "/issues",
+    },
+  ]
