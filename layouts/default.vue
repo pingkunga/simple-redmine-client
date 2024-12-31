93b45d4c0be3113997a7a78bd5d783bb253dea28
Chatri Ngambenchawong <pingkunga@gmail.com>
2024-12-31 08:30:20 +0700

    feat: add layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,27 +1,21 @@
 <template>
-    <v-app>
-        <!-- Header -->
-        <v-app-bar>
-            <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon> -->
-            <v-toolbar-title>Redmine Client Tools</v-toolbar-title>
-        </v-app-bar>
-       
-        <v-main>
-            <!-- Sidebar -->
-            <v-navigation-drawer
-                    left
-                    elevation="10"
-                    app
-                    expand-on-hover
-                >
-                Sidebar
-            </v-navigation-drawer>
+  <v-app>
+    <!-- Header -->
+    <v-app-bar>
+      <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon> -->
+      <v-toolbar-title>Redmine Client Tools</v-toolbar-title>
+    </v-app-bar>
 
-            <!-- Main content -->
-            <v-container fluid class="page-wrapper">
-                <NuxtPage />
-            </v-container>
-        </v-main>
-    </v-app>
-  </template>
-  
+    <v-main>
+      <!-- Sidebar -->
+      <v-navigation-drawer left elevation="10" app expand-on-hover>
+        <sidebar />
+      </v-navigation-drawer>
+
+      <!-- Main content -->
+      <v-container fluid class="page-wrapper">
+        <NuxtPage />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
