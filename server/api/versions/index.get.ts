c2864b808954db30e317a50e111a5075e578757e
Chatri Ngambenchawong <pingkunga@gmail.com>
2025-01-04 18:29:49 +0700

    refactor: clear comment

--- server/api/versions/index.get.ts
+++ server/api/versions/index.get.ts
@@ -3,52 +3,6 @@ import useRedmineAPI from "~/composables/useRedmineAPI";
 
 export default defineEventHandler(async (event) => {
   
-  /*
-    //Get Version from redmine
-    const config = useRuntimeConfig(event);
-
-    const param = {
-        baseUrl: config.redmineUrl,
-        token: config.redmineToken,
-    }
-    
-    const response = await fetch(`${param.baseUrl}/projects/858/versions.json`, {
-        headers: {
-            'X-Redmine-API-Key': param.token
-        }
-    });
-
-    if (!response.ok) {
-        throw new Error(`Error fetching Redmine version: ${response.statusText}`);
-    }
-
-    const data = await response.json();
-    //console.log(data);
-
-    const versionsData: RawVersion[] = data.versions.map((version: any) => ({
-        id: version.id,
-        project: {
-          id: version.project.id,
-          name: version.project.name
-        },
-        name: version.name,
-        description: version.description,
-        status: version.status,
-        due_date: version.due_date,
-        sharing: version.sharing,
-        wiki_page_title: version.wiki_page_title,
-        created_on: version.created_on,
-        updated_on: version.updated_on
-      }));
-    
-      const versionsResponse: VersionsResponse = {
-        versions: versionsData,
-        total_count: data.total_count
-      };
-    
-    //console.log(versionsResponse);
-    return versionsResponse;
-*/
     const versionsData: Version[] = [];
     const config = useRuntimeConfig(event);
 
