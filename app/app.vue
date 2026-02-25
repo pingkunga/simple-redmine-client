9fe27dee9ada7123782a73c79cdba83f75c1ac50
pingkunga <pingkunga@gmail.com>
2026-02-25 21:05:40 +0700

    feat: debug for view env issue

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+//const config = useRuntimeConfig()
+//console.log('Runtime Config:', config)
+</script>
 <template>
   <UApp :toaster="{ position: 'top-right', duration: 2000 }">
     <NuxtLayout>
